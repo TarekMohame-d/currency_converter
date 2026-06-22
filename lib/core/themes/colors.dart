bb40d2508da1db132ae8ff1ad16c0e7aea16b9ea
Tarek Mohamed <tarekmohamed1019@gmail.com>
2026-06-22 12:44:27 +0300

    Added app default theme

---
+++ lib/core/themes/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class KColors {
+  KColors._();
+  static const Color scaffoldLightColor = Color(0xFFF6F6F6);
+  static const Color scaffoldDarkColor = Color(0xFF252836);
+
+  // Primary
+  static const Color primaryColor = Color(0xFF3a5a40);
+  static const Color soft = Color(0xFF252836);
+
+  // Secondary
+
+  // Text Color
+  static const Color black = Colors.black;
+  static const Color darkGrey = Color(0xFF696974);
+  static const Color grey = Colors.grey;
+  static const Color whiteGrey = Color(0xFFF1F1F5);
+  static const Color white = Colors.white;
+  static const Color lineDark = Color(0xFFEAEAEA);
+}
